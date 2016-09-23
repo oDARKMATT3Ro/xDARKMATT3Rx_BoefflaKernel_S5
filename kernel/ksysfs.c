1d41292da988b109b77c834c57ca2cbc881c1246
xDARKMATT3Rx <geoff.goeres@gmail.com>
2016-09-23 10:04:49 -0700

    sched: add controls for sched features

--- kernel/ksysfs.c
+++ kernel/ksysfs.c
@@ -215,11 +215,13 @@ static struct attribute_group kernel_attr_group = {
 static unsigned int Lgentle_fair_sleepers = 0;
 static unsigned int Larch_power = 0;
 extern void relay_gfs(unsigned int gfs);
+extern void relay_ap(unsigned int ap);
 
 static ssize_t gentle_fair_sleepers_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
 	return sprintf(buf, "%u\n", Lgentle_fair_sleepers);
 }
+
 static ssize_t gentle_fair_sleepers_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
 	unsigned int input;
@@ -265,12 +267,11 @@ static struct attribute_group sched_features_attr_group = {
 };
 
 /* Initialize fast charge sysfs folder */
-static struct kobject *gentle_fair_sleepers_kobj;
+static struct kobject *sched_features_kobj;
 
 static int __init ksysfs_init(void)
 {
 	int error;
-	int retval;
 
 	kernel_kobj = kobject_create_and_add("kernel", NULL);
 	if (!kernel_kobj) {
@@ -281,11 +282,11 @@ static int __init ksysfs_init(void)
 	if (error)
 		goto kset_exit;
 
-	gentle_fair_sleepers_kobj = kobject_create_and_add("sched", kernel_kobj);
-	retval = sysfs_create_group(gentle_fair_sleepers_kobj, &gentle_fair_sleepers_attr_group);
+	sched_features_kobj = kobject_create_and_add("sched", kernel_kobj);
+		error = sysfs_create_group(sched_features_kobj, &sched_features_attr_group);
 
-	if (retval)
-		kobject_put(gentle_fair_sleepers_kobj);
+	if (error)
+		kobject_put(sched_features_kobj);
 
 	if (notes_size > 0) {
 		notes_attr.size = notes_size;
